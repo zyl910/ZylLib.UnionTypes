6b5fa4e8dc50503e76ce90cf06076197a74dc32e
zyl910 <zyl910@sina.com>
2017-08-02 16:18:14 +0800

    Add project: ZylLib.UnionTypes.ConsoleExample.net20.csproj

---
+++ Examples/ZylLib.UnionTypes.ConsoleExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZylLib.UnionTypes.ConsoleExample {
+	class Program {
+		static void Main(string[] args) {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("ZylLib.UnionTypes.ConsoleExample");
+			sb.AppendLine("");
+			// show.
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
