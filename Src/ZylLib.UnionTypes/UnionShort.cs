d34bbffa9963e52c2a543aaafa919aa762b06399
zyl910 <zyl910@sina.com>
2017-08-02 16:52:48 +0800

    Coding UnionShort class.

--- Src/ZylLib.UnionTypes/UnionShort.cs
+++ Src/ZylLib.UnionTypes/UnionShort.cs
@@ -1,8 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace ZylLib.UnionTypes {
-	public class UnionShort {
+	/// <summary>
+	/// 2 byte union type. Example `short[1]`
+	/// </summary>
+	/// 
+	/// <namespace>
+	/// <summary>
+	/// Union types in C# (C#中的联合体类型)
+	/// </summary>
+	/// </namespace>
+	[Serializable]
+	[StructLayout(LayoutKind.Explicit, Size = 2)]
+	public struct UnionShort {
+		/// <summary>short(Int16) 0</summary>
+		[FieldOffset(0)]
+		public short S0;
+
+		/// <summary>ushort(UInt16) 0</summary>
+		[CLSCompliant(false)]
+		[NonSerialized]
+		[FieldOffset(0)]
+		public ushort US0;
+
+		/// <summary>byte(Byte) 0</summary>
+		[NonSerialized]
+		[FieldOffset(0)]
+		public byte B0;
+		/// <summary>byte(Byte) 1</summary>
+		[NonSerialized]
+		[FieldOffset(1)]
+		public byte B1;
+
+		/// <summary>
+		/// Create UnionShort, params has short type.
+		/// </summary>
+		/// <param name="v">Value</param>
+		public UnionShort(short v) {
+			US0 = 0;
+			B0 = 0;
+			B1 = 0;
+			// set.
+			S0 = v;
+		}
+
+		/// <summary>
+		/// Create UnionShort, params has ushort type.
+		/// </summary>
+		/// <param name="v">Value</param>
+		[CLSCompliant(false)]
+		public UnionShort(ushort v) {
+			S0 = 0;
+			B0 = 0;
+			B1 = 0;
+			// set.
+			US0 = v;
+		}
+
 	}
 }
