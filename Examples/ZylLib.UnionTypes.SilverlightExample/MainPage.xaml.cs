9916f1b2a4f268a15ac22dbb10850d5abfe466b0
zyl910 <zyl910@sina.com>
2017-08-03 18:02:47 +0800

    ZylLib.UnionTypes.SilverlightExample.sl40.csproj

---
+++ Examples/ZylLib.UnionTypes.SilverlightExample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ZylLib.UnionTypes;
+
+namespace ZylLib.UnionTypes.SilverlightExample.sl40 {
+	public partial class MainPage : UserControl {
+		public MainPage() {
+			InitializeComponent();
+		}
+
+		private void btnInfo_Click(object sender, RoutedEventArgs e) {
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("ZylLib.UnionTypes.SilverlightExample");
+			sb.AppendLine();
+			UnionTypesExample.Output(sb);
+			// show.
+			txtOut.Text = sb.ToString();
+		}
+	}
+}
---
+++ Examples/ZylLib.UnionTypes.SilverlightExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的常规信息通过以下
+// 特性集控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("ZylLib.UnionTypes.SilverlightExample.sl40")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZylLib.UnionTypes.SilverlightExample.sl40")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 可使此程序集中的类型
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+// 则将该类型上的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则以下 GUID 用作类型库的 ID
+[assembly: Guid("b2668f6a-407b-44fc-9518-732e701f81c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+// See AssemblyVer.cs
+
+//[assembly: CLSCompliant(true)]
+
